08b0a583bcdec15cb7bf442171c0ead72ac43455
Tatiana Titova <ritasom666@gmail.com>
2024-01-31 19:54:17 +0400

    add new tests

--- src/test/java/ru/netology/test/TodosTest.java
+++ src/test/java/ru/netology/test/TodosTest.java
@@ -10,15 +10,25 @@ import ru.netology.domain.Todos;
 
 public class TodosTest {
     SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+    SimpleTask simpleTask2 = new SimpleTask(6, "Позвонить папе");
 
     String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
     Epic epic = new Epic(55, subtasks);
+    String[] subtasks2 = {"Яблоки", "Мясо", "Молоко"};
+    Epic epic2 = new Epic(66, subtasks2);
 
     Meeting meeting = new Meeting(
             555,
             "Выкатка 3й версии приложения",
             "Приложение НетоБанка",
             "Во вторник после обеда"
+    );
+
+    Meeting meeting2 = new Meeting(
+            666,
+            "Согласование новой версии",
+            "Сайт НетоБанка",
+            "В четверг утром"
     );
     Todos todos = new Todos();
 
@@ -35,11 +45,23 @@ public class TodosTest {
 
 
     @Test
-    public void shouldSearchSimpleTask() {
+    public void shouldSearchOneSimpleTask() {
 
         todos.search("родителям");
         boolean actual = simpleTask.matches("родителям");
+        boolean actual2 = simpleTask2.matches("родителям");
         Assertions.assertTrue(actual);
+        Assertions.assertFalse(actual2);
+    }
+
+    @Test
+    public void shouldSearchMoreOneSimpleTask() {
+        todos.search("Позвонить");
+        boolean actual = simpleTask.matches("Позвонить");
+        boolean actual2 = simpleTask2.matches("Позвонить");
+        Assertions.assertTrue(actual);
+        Assertions.assertTrue(actual2);
+
     }
 
     @Test
@@ -47,35 +69,74 @@ public class TodosTest {
 
         todos.search("маме");
         boolean actual = simpleTask.matches("маме");
+        boolean actual2 = simpleTask2.matches("маме");
         Assertions.assertFalse(actual);
+        Assertions.assertFalse(actual2);
+
     }
 
     @Test
-    public void shouldSearchEpicTask() {
+    public void shouldSearchOneEpicTask() {
         todos.search("Хлеб");
         boolean actual = epic.matches("Хлеб");
+        boolean actual2 = epic2.matches("Хлеб");
         Assertions.assertTrue(actual);
+        Assertions.assertFalse(actual2);
+    }
+
+    @Test
+    public void shouldSearchMoreOneEpicTask() {
+        todos.search("Молоко");
+        boolean actual = epic.matches("Молоко");
+        boolean actual2 = epic2.matches("Молоко");
+        Assertions.assertTrue(actual);
+        Assertions.assertTrue(actual2);
     }
 
     @Test
     public void shouldNotSearchEpicTask() {
         todos.search("сок");
         boolean actual = epic.matches("сок");
+        boolean actual2 = epic2.matches("сок");
         Assertions.assertFalse(actual);
+        Assertions.assertFalse(actual2);
     }
 
     @Test
-    public void shouldSearchMeetingTaskInTopic() {
+    public void shouldSearchOneMeetingTaskInTopic() {
         todos.search("Выкатка");
         boolean actual = meeting.matches("Выкатка");
+        boolean actual2 = meeting2.matches("Выкатка");
         Assertions.assertTrue(actual);
+        Assertions.assertFalse(actual2);
     }
 
     @Test
-    public void shouldSearchMeetingTaskInProject() {
+    public void shouldSearchMoreOneMeetingTaskInTopic() {
+        todos.search("версии");
+        boolean actual = meeting.matches("версии");
+        boolean actual2 = meeting2.matches("версии");
+        Assertions.assertTrue(actual);
+        Assertions.assertTrue(actual2);
+    }
+
+    @Test
+    public void shouldSearchOneMeetingTaskInProject() {
         todos.search("Приложение");
         boolean actual = meeting.matches("Приложение");
+        boolean actual2 = meeting2.matches("Приложение");
         Assertions.assertTrue(actual);
+        Assertions.assertFalse(actual2);
+
+    }
+
+    @Test
+    public void shouldSearchMoreOneMeetingTaskInProject() {
+        todos.search("НетоБанка");
+        boolean actual = meeting.matches("НетоБанка");
+        boolean actual2 = meeting2.matches("НетоБанка");
+        Assertions.assertTrue(actual);
+        Assertions.assertTrue(actual2);
 
     }
 
@@ -83,7 +144,9 @@ public class TodosTest {
     public void shouldNotSearchMeetingTack() {
         todos.search("созвон");
         boolean actual = meeting.matches("созвон");
+        boolean actual2 = meeting2.matches("созвон");
         Assertions.assertFalse(actual);
+        Assertions.assertFalse(actual2);
     }
 }
 
